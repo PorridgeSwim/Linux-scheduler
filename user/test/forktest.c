7a1dec1ee01788a58052e2d7fd1602fc44940c2a
AsterWei <aw3389@columbia.edu>
2022-04-05 15:47:56 -0400

    add fork test

--- user/test/forktest.c
+++ user/test/forktest.c
@@ -1,15 +1,52 @@
+﻿#include <stdio.h>      /* for printf() and fprintf() */
+#include <stdlib.h>     /* for atoi() and exit() */
+#include <unistd.h>     /* for close() */
+#include <time.h>       /* for time() */
+#include <signal.h>     /* for signal() */
+#include <sys/stat.h>   /* for stat() */
+#include <sys/wait.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <sched.h>
+#include <string.h>
 
-
-
-int main(){
+int main( int argc, char *argv[] ){
 	//fork when receive keyboard interrupt
-	pid_t pid;
-	if(fork()==0){
-		sleep(10);
-
-
+	pid_t parpid = getpid();
+	int ret;
+	int i;
+	int j;
+	int k;
+	int sched_policy;
+	struct sched_param freezer_param;
+	if(argc == 2){
+		sched_policy = sched_getscheduler(parpid);
+		freezer_param.sched_priority = 0;
+		ret = sched_setscheduler(parpid, 7, &freezer_param);
+		if(ret == 0){
+         printf("[%d] sched policy changed: %d --> %d\n", parpid, sched_policy, 7);
+		}
+		else{
+			printf("%s \n", strerror(errno));
+		}
+		int count = strtol(argv[1], NULL, 10);
+		pid_t pid[count];
+		for(i = 0; i<count; i++) {
+			pid[i] = fork();
+			if(!pid[i]){
+				for(k = 1; k < 10000000; k++){}
+				return 0;
+			}
+		}
+		for(j = 0; j < count; j++) {
+			waitpid(pid[j], 0, 0);
+		}
 	}
-	else{
-		
-	}
+	
+   else {
+      printf("One argument expected.\n");
+   }
+   return 0;
 }
